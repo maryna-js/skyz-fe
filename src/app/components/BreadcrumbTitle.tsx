822c6b96531c26ce7abfe4e3130e208c0304b39a
maryna-js <rastargueva.m@gmail.com>
2024-04-15 19:43:19 +0200

    feat: component with breadcrumb and title mobile breakpoint

--- src/app/components/BreadcrumbTitle.tsx
+++ src/app/components/BreadcrumbTitle.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const BreadcrumbTitle: React.FC = () => {
   return (
-    <div className="py-4 4xl:py-20 px-8 4xl:px-24">
+    <div className="pt-8 pb-4 lg:py-4 4xl:py-20 px-6 lg:px-8 4xl:px-24">
       <nav className="mr-4 text-xs text-river-bed">
        <span>MYEDGE</span>
        <span className="px-2">/</span>
@@ -10,11 +10,11 @@ const BreadcrumbTitle: React.FC = () => {
        <span className="px-2">/</span>
        <span className="text-white-lilac">REBELS VS RIDERS</span>
       </nav>
-      <div className="flex items-center justify-between pt-3">
-        <h1 className="text-4xl font-bold text-white">UPCOMING LEAGUE OF LEGENDS MATCHES</h1>
-        <div className="justify-between -ml-6">
-            <button className="bg-malibu text-blue-dianne uppercase text-sm px-4 py-2 hover:bg-curious-blue rounded-lg">UPCOMING</button>
-            <button className="bg-woodsmoke text-abbey uppercase text-sm px-4 py-2 hover:bg-cod-gray-opacity rounded-lg">Finished</button>
+      <div className="flex-col lg:flex-row flex items-center justify-between pt-3">
+        <h1 className="text-lg lg:text-4xl font-bold text-white">UPCOMING LEAGUE OF LEGENDS MATCHES</h1>
+        <div className="justify-between pt-6 4xl:pt-0 ml-0 4xl:-ml-6 w-full lg:w-auto">
+            <button className="w-1/2 lg:w-auto bg-malibu text-blue-dianne uppercase text-sm px-4 py-2 hover:bg-curious-blue rounded-lg">UPCOMING</button>
+            <button className="w-1/2 lg:w-auto bg-woodsmoke text-abbey uppercase text-sm px-4 py-2 hover:bg-cod-gray-opacity rounded-lg">Finished</button>
         </div>
       </div>
     </div>
